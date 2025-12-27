77a09ed060e504559ca51ed8d449f6b52d43b701
etter47582 <etter0000@gmail.com>
2025-12-27 15:08:43 +0200

    Initial commit

---
+++ LivitaTestTask/ContentView.swift
@@ -0,0 +1,19 @@
+//(c) Copyright PopAppFactory 2023
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LivitaTestTask/LivitaTestTaskApp.swift
@@ -0,0 +1,12 @@
+//(c) Copyright PopAppFactory 2023
+
+import SwiftUI
+
+@main
+struct LivitaTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
